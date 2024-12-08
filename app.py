c2c7ba712ca9c51adcbc6425df82913b09a60cef
wasimhassanshah <sayedwasimshahkazmi@gmail.com>
2024-12-08 21:30:34 +0500

    added

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def index():
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port = 8080, debug=True)
+	app.run(host="0.0.0.0", port = 8080, debug=True) # If you are deploying mode, remove this debug=True
