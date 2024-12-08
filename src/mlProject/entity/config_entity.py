f8c304a4e0f30c34688e968c160547e0beb6fb03
wasimhassanshah <sayedwasimshahkazmi@gmail.com>
2024-12-08 19:59:44 +0500

    model_evaluation added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -31,3 +31,17 @@ class ModelTrainerConfig:
     alpha: float   #these two parameters (alpha and l1_ratio) are from params.yaml
     l1_ratio: float      
     target_column: str   #this is from schema.yaml
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
+    mlflow_uri: str
